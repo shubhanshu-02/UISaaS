fba05fbab0b9585f9b2b5e8f224c984f976fb86c
shubhanshu-02 <shubhanshu004@gmail.com>
2025-08-19 02:15:56 +0530

    restarting, hopefully all good

---
+++ apps/dashboard/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import '/globals.css';
+import { ReactNode } from 'react';
+import { AuthProvider } from '../components/AuthProvider';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}

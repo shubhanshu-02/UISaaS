991ebbf7c65c868a92048a2863cd838ac663fc15
shubhanshu-02 <shubhanshu004@gmail.com>
2025-08-22 02:53:34 +0530

    misc

--- apps/api/types/next-auth.d.ts
+++ apps/api/types/next-auth.d.ts
@@ -1,28 +1,34 @@
-import NextAuth from "next-auth";
+// types/next-auth.d.ts
+import NextAuth from "next-auth"
+import { JWT } from "next-auth/jwt"
+import type { User as CustomUser } from '@ui-saas/types'
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string;
-      tier: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+      id: string
+      name?: string | null
+      email?: string | null
+      image?: string | null
+      tier: CustomUser['tier']
+      isDemo: boolean
+    }
   }
+
   interface User {
-    id: string;
-    tier: string;
-    name?: string | null;
-    email?: string | null;
-    image?: string | null;
+    id: string
+    name?: string | null
+    email?: string | null
+    image?: string | null
+    tier: CustomUser['tier']
+    isDemo: boolean
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id?: string;
-    tier?: string;
+    id: string
+    tier: CustomUser['tier']
+    isDemo: boolean
   }
 }
-    
